dd257ebc60ea92c750389662bb27c8181c2412da
Rasso Hilber <mail@rassohilber.com>
2023-11-30 17:19:48 +0100

    Add tests

--- tests/playwright/main.spec.ts
+++ tests/playwright/main.spec.ts
@@ -1,13 +1,45 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
+import { scrollTo, expectScrollPosition, sleep } from "./support";
 
+test.describe("Vanilla Scroll Sync", () => {
+  test.beforeEach(async ({ page }) => {
+    page.setViewportSize({ width: 1000, height: 1000 });
+    await page.goto("/");
+  });
 
-test.describe('Vanilla Scroll Sync', () => {
-	test.beforeEach(async ({ page }) => {
-		await page.goto('/');
+  test("should mirror vertical scroll positions", async ({ page }) => {
+    await page.getByTestId("first-vertical").scrollIntoViewIfNeeded();
+    await scrollTo(page, { y: 3000 }, "first-vertical");
+    await sleep(1000);
+    expect(page.getByTestId("third-vertical-end")).toBeInViewport();
+  });
 
-	});
+  test("should mirror horizontal scroll positions", async ({ page }) => {
+    page.setViewportSize({ width: 1000, height: 1000 });
+    await page.getByTestId("third-horizontal").scrollIntoViewIfNeeded();
+    await scrollTo(page, { x: 10000 }, "third-horizontal");
+    await sleep(1000);
+    await expect(page.getByTestId("first-horizontal-end")).toBeInViewport();
+  });
 
-	test('should test stuff', async ({ page }) => {
+  test("should mirror positions in both directions", async ({ page }) => {
+    page.setViewportSize({ width: 1000, height: 1000 });
+    await page.getByTestId("first-both").scrollIntoViewIfNeeded();
+    await scrollTo(page, { x: 10000, y: 10000 }, "first-both");
+    await sleep(1000);
+    await expect(page.getByTestId("third-both-end")).toBeInViewport();
+  });
 
-	});
+  test("should mirror scroll positions between the window and elements", async ({ page }) => {
+    // window > element
+    page.setViewportSize({ width: 1400, height: 1000 });
+    await scrollTo(page, { y: 100000 });
+    await sleep(1000);
+    await expect(page.getByTestId("scroller-fixed-end")).toBeInViewport();
+    // element > window
+    await scrollTo(page, { y: 0 }, "scroller-fixed");
+    await sleep(1000);
+    await expectScrollPosition(page, {x: 0, y: 0});
+  });
+
 });
---
+++ tests/playwright/support.ts
@@ -0,0 +1,59 @@
+import { expect } from "@playwright/test";
+import type { Page } from "@playwright/test";
+
+type ScrollPosition = {
+  x: number;
+  y: number;
+};
+
+export function scrollTo(
+  page: Page,
+  position: Partial<ScrollPosition>,
+  testId?: string
+) {
+  return page.evaluate(
+    (args) => {
+      if (args.testId) {
+        window.document
+          .querySelector(`[data-testid="${args.testId}"]`)
+          ?.scrollTo(args.position.x, args.position.y);
+        return;
+      }
+      window.scrollTo(args.position.x, args.position.y);
+    },
+    {
+      position: {
+        ...{ x: 0, y: 0 },
+        ...position,
+      },
+      testId,
+    }
+  );
+}
+
+export function sleep(timeout = 0): Promise<void> {
+  return new Promise((resolve) =>
+    setTimeout(() => resolve(undefined), timeout)
+  );
+}
+
+export async function expectScrollPosition(
+  page: Page,
+  expected: ScrollPosition,
+  testId?: string
+) {
+  const scrollY = await page.evaluate((testId): ScrollPosition => {
+    if (!testId)
+      return {
+        x: window.scrollY,
+        y: window.scrollX,
+      };
+    const el = window.document.querySelector(`[data-testid="${testId}"]`);
+    if (!el) return { x: -1, y: -1 };
+    return {
+      x: el.scrollLeft,
+      y: el.scrollTop,
+    };
+  }, testId);
+  expect(scrollY).toEqual(expected);
+}
