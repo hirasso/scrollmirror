aa560c77c45676a2fbf502f6fb3a85c8c92804a1
Rasso Hilber <mail@rassohilber.com>
2023-12-02 11:17:02 +0100

    docs WIP

--- docs/src/js/global.ts
+++ docs/src/js/global.ts
@@ -2,9 +2,9 @@ import ScrollMirror from "../../../src/index.js";
 /** Vertical mirroring */
 new ScrollMirror(document.querySelectorAll(".scroller.--vertical"));
 /** Horizontal mirroring */
-new ScrollMirror(document.querySelectorAll(".scroller--horizontal"));
+new ScrollMirror(document.querySelectorAll(".scroller.--horizontal"));
 /** Mirroring in both directions */
-new ScrollMirror(document.querySelectorAll(".scroller--both"));
+new ScrollMirror(document.querySelectorAll(".scroller.--both"));
 /** Mirroring with the window */
 new ScrollMirror([
   document.body,
